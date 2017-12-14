6fae7909be9008ac2d45b5eb99fb2dea3a89d813
Ali Faradji <23017641+faradji@users.noreply.github.com>
2017-12-14 11:44:42 +0100

    Fix for this error
    Error: ER_LOCK_DEADLOCK: Deadlock found when trying to get lock; try restarting transaction
        at Query.Sequence._packetToError (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mysql/lib/protocol/sequences/Sequence.js:52:14)
        at Query.ErrorPacket (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mysql/lib/protocol/sequences/Query.js:77:18)
        at Protocol._parsePacket (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mysql/lib/protocol/Protocol.js:279:23)
        at Parser.write (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mysql/lib/protocol/Parser.js:76:12)
        at Protocol.write (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mysql/lib/protocol/Protocol.js:39:16)
        at Socket.<anonymous> (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mysql/lib/Connection.js:103:28)
        at emitOne (events.js:116:13)
        at Socket.emit (events.js:211:7)
        at addChunk (_stream_readable.js:263:12)
        at readableAddChunk (_stream_readable.js:250:11)
        --------------------
        at Pool.query (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mysql/lib/Pool.js:199:23)
        at myLimiterMiddleware (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/mylimiter/index.js:20:14)
        at Layer.handle [as handle_request] (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/express/lib/router/layer.js:95:5)
        at trim_prefix (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/express/lib/router/index.js:317:13)
        at /home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/express/lib/router/index.js:284:7
        at param (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/express/lib/router/index.js:354:14)
        at param (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/express/lib/router/index.js:365:14)
        at Function.process_params (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/express/lib/router/index.js:410:3)
        at next (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/node_modules/express/lib/router/index.js:275:10)
        at server.use (/home/ubuntu/skrivbord/git-repo/projectx/projectx-server/node/lib/instantiate-server.js:101:3)

--- index.js
+++ index.js
@@ -1,35 +1,78 @@
 "use strict";
 
-var log = require('ssi-logger');
-var mysql = require('mysql');
+const log = require( 'ssi-logger' );
+const mysql = require( 'mysql' );
+let connection;
+module.exports = function myLimiter( poolOrConfig, bucketOrFunction, bucketSizeOrFunction, periodInSecondsOrFunction ) {
 
-module.exports = function myLimiter(poolOrConfig, bucketOrFunction, bucketSizeOrFunction, periodInSecondsOrFunction) {
+	const pool = poolOrConfig.hasOwnProperty( 'config' ) ? poolOrConfig : mysql.createPool( poolOrConfig );
 
-    var pool = poolOrConfig.hasOwnProperty('config') ? poolOrConfig : mysql.createPool(poolOrConfig);
+	return async function myLimiterMiddleware( req, res, next ) {
 
-    return function myLimiterMiddleware(req, res, next) {
+		const bucket = ( typeof bucketOrFunction === "function" ) ? bucketOrFunction( req, res ) : bucketOrFunction;
+		const bucketSize = ( typeof bucketSizeOrFunction === "function" ) ? bucketSizeOrFunction( req, res ) : bucketSizeOrFunction;
+		const periodInSeconds = ( typeof periodInSecondsOrFunction === "function" ) ? periodInSecondsOrFunction( req, res ) : periodInSecondsOrFunction;
 
-        var bucket = (typeof bucketOrFunction === "function") ? bucketOrFunction(req, res) : bucketOrFunction;
-        var bucketSize = (typeof bucketSizeOrFunction === "function") ? bucketSizeOrFunction(req, res) : bucketSizeOrFunction;
-        var periodInSeconds = (typeof periodInSecondsOrFunction === "function") ? periodInSecondsOrFunction(req, res) : periodInSecondsOrFunction;
+		if ( arguments.length === 1 ) {
+			next = arguments[ 0 ];
+		}
 
-        if (arguments.length === 1) {
-            next = arguments[0];
-        }
+		pool.getConnection( async function ( getConnectionErr, connectionFromPool ) {
+			if ( getConnectionErr ) {
+				log( 'DEBUG', 'mylimiter', getConnectionErrr );
+			}
+			try {
+				connection = await connectionFromPool;
+			} catch ( e ) {
+				log( 'DEBUG', 'mylimiter', e );
+			}
 
-        pool.query('CALL drip(?, ?, ?)', [ bucket, bucketSize, periodInSeconds ] , function (err, result) {
-            if (err) {
-                err.status = (err.sqlState === '45429') ? 429 : 500;
-                if (err.status === 429) {
-                    log('WARN', 'mylimiter limit exceeded bucket=%s', bucket);
-                } else {
-                    log('ERR', 'mylimiter db error bucket=%s', bucket, err);
-                }
-                next(err);
-                return;
-            }
-            log('DEBUG', 'mylimiter ok bucket=%s', bucket);
-            next();
-        });
-    };
+			connection.beginTransaction( function ( transactionErr ) {
+				if ( transactionErr ) {
+					connection.rollback( function () {
+						connection.release();
+						log( 'DEBUG', 'mylimiter', transactionErr );
+					} );
+				}
+
+				connection.query( 'CALL drip(?, ?, ?)', [ bucket, bucketSize, periodInSeconds ], function ( queryErr, result ) {
+					connection.release();
+					if ( queryErr ) {
+						queryErr.status = ( queryErr.sqlState === '45429' ) ? 429 : 500;
+						if ( queryErr.status === 429 ) {
+							connection.rollback( function () {
+								connection.release();
+								log( 'DEBUG', 'mylimiter', commitErr );
+							} );
+							log( 'WARN', 'mylimiter limit exceeded bucket=%s', bucket );
+						} else {
+							connection.rollback( function () {
+								connection.release();
+								log( 'DEBUG', 'mylimiter', commitErr );
+							} );
+							connection.rollback( function () {
+								connection.release();
+								log( 'DEBUG', 'mylimiter', commitErr );
+							} );
+							log( 'ERR', 'mylimiter db error bucket=%s', bucket, queryErr );
+						}
+						next( queryErr );
+						return;
+					}
+					connection.commit( function ( commitErr ) {
+						if ( commitErr ) {
+							connection.rollback( function () {
+								connection.release();
+								log( 'DEBUG', 'mylimiter', commitErr );
+							} );
+						}
+						log( 'DEBUG', 'mylimiter ok bucket=%s', bucket );
+						next();
+
+					} );
+
+				} );
+			} );
+		} );
+	};
 };
